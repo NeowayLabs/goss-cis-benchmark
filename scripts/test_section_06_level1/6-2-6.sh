ffc13d5a00fd7f02a77d53145ac187726604e8fa
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2018-10-04 21:23:29 -0300

    Fix and improve 6-2-6.sh

--- scripts/test_section_06_level1/6-2-6.sh
+++ scripts/test_section_06_level1/6-2-6.sh
@@ -1,29 +1,59 @@
 #!/bin/bash
-if [ "`echo $PATH | grep :: `" != "" ]; then
+
+set -o errexit
+set -o nounset
+
+declare dirown=""
+declare dirperm=""
+declare p=""
+declare path=""
+declare status="0"
+
+echo ${PATH} | grep "::" > /dev/null 2>&1 && status=1
+
+if [ ${status} != "0" ]; then
    echo "Empty Directory in PATH (::)"
+   exit 1
 fi
-if [ "`echo $PATH | grep :$`"  != "" ]; then
+
+echo $PATH | grep ":$" > /dev/null 2>&1 && status=1
+
+if [ ${status} != "0" ]; then
  echo "Trailing : in PATH"
+ exit 1
 fi
-p=`echo $PATH | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'` set -- $p
-while [ "$1" != "" ]; do
- if [ "$1" = "." ]; then
-   echo "PATH contains ."
-   shift
-   continue
- fi
- if [ -d $1 ]; then
-dirperm=`ls -ldH $1 | cut -f1 -d" "`
-if [ `echo $dirperm | cut -c6 ` != "-" ]; then
-echo "Group Write permission set on directory $1" fi
-if [ `echo $dirperm | cut -c9 ` != "-" ]; then
-echo "Other Write permission set on directory $1"
-   fi
-   dirown=`ls -ldH $1 | awk '{print $3}'`
-   if [ "$dirown" != "root" ] ; then
-     echo $1 is not owned by root
-   fi else
- echo $1 is not a directory
-fi
-shift
+
+path=$(echo ${PATH} | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g')
+set -- ${path}
+while [ "${1-}" != "" ]; do
+    p=${1-}
+    if [ "${p}" = "." ]; then
+        echo "PATH contains ."
+        shift
+        continue
+    fi
+    if [ -d "${p}" ]; then
+        dirperm=$(ls -ldH ${p} | cut -f1 -d" ")
+        echo ${dirperm} | cut -c6 | grep "-" > /dev/null 2>&1 || status=1
+        if [ ${status} != "0" ]; then
+            echo "Group Write permission set on directory ${p}" fi
+            exit 1
+        fi
+        echo ${dirperm} | cut -c9 | grep "-" > /dev/null 2>&1 || status=1
+        if [ ${status} != "0" ]; then
+            echo "Other Write permission set on directory ${p}"
+            exit 1
+        fi
+        dirown=$(ls -ldH ${p} | awk '{print $3}') || status=1
+        if [ ${status} = "0" ]; then
+            if [ "${dirown}" != "root" ] ; then
+                echo "${p} is not owned by root"
+                exit 1
+            fi
+        fi
+    else
+        echo "${p} is not a directory"
+        exit 1
+    fi
+    shift
 done
