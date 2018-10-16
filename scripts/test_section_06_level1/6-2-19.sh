29679669309f316621d82f6043577219ef1240e0
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2018-10-16 18:32:20 -0300

    Improve 6-2-19.sh

--- scripts/test_section_06_level1/6-2-19.sh
+++ scripts/test_section_06_level1/6-2-19.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
-cat /etc/group | cut -f1 -d":" | sort -n | uniq -c | while read x ; do [ -z "${x}" ] && break
-set - $x
-if [ $1 -gt 1 ]; then
-gids=`gawk -F: '($1 == n) { print $3 }' n=$2 /etc/group | xargs` echo "Duplicate Group Name ($2): ${gids}"
-fi done
+#
+# 6.2.19 Ensure no duplicate group names exist
+#
+# Description:
+# Although the groupadd program will not let you create a duplicate
+# group name, it is possible for an administrator to manually edit
+# the /etc/group file and change the group name.
+
+set -o errexit
+set -o nounset
+
+declare gids=""
+declare group=""
+declare l=""
+declare line=""
+declare qtd=""
+declare status="0"
+declare stderr="0"
+
+line=$(cut -f1 -d":" /etc/group | sort -n | uniq -c | awk '{printf $1" "$2"@"}' |sed 's#@$##g') || status=1
+
+if [ ${status} = "0" ]; then
+
+    IFS="@"
+    for l in ${line}; do
+            IFS=" " && set - ${l}
+            qtd=${1-} && group=${2-}
+            if [ ${qtd} -gt 1 ]; then
+                gids=$(awk -F: '($1 == n) { print $3 }' n=${group} /etc/group | xargs)
+                echo "Duplicate Group Name (${group}): ${gids}"
+                stderr="1"
+            fi
+    done
+
+fi
+
+if [ ${stderr} != "0" ]; then
+    exit 1
+fi
