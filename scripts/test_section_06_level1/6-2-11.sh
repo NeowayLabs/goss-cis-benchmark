5d902f243fb7c81e204d2e2c9c9691a75cd4c89d
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2018-10-16 10:42:55 -0300

    Improve 6-2-11.sh

--- scripts/test_section_06_level1/6-2-11.sh
+++ scripts/test_section_06_level1/6-2-11.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
-cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
-if [ ! -d "$dir" ]; then
-echo "The home directory ($dir) of user $user does not exist."
-else
-if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then
-echo ".forward file $dir/.forward exists"
-fi fi
-done
+#
+# 6.2.11 Ensure no users have .forward files
+#
+# Description:
+# The .forward file specifies an email address to forward the user's mail to.
+
+set -o errexit
+set -o nounset
+
+declare dir=""
+declare line=""
+declare status="0"
+declare stderr="0"
+declare user=""
+declare vars=""
+
+while read line; do
+
+    vars=$(
+            echo ${line} | \
+            egrep -v '^(root|halt|sync|shutdown)' | \
+            awk -F: '($7 != "/usr/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }'
+          ) || status=1
+
+    if [ ${status} = "0" -a "${vars}x" != "x" ]; then
+        set -- ${vars}
+        user=${1-} && dir=${2-}
+        if [ ! -d ${dir} ]; then
+            echo "The home directory (${dir}) of user ${user} does not exist."
+            stderr="1"
+        else
+            if [ ! -h "${dir}/.forward" -a -f "${dir}/.forward" ]; then
+                echo ".forward file ${dir}/.forward exists"
+                stderr="1"
+            fi
+        fi
+    fi
+
+done < /etc/passwd
+
+if [ ${stderr} != "0" ]; then
+    exit 1
+fi
