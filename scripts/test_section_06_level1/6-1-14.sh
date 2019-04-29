d5b3cb7a072ad5650b70d11450a3b1043ed1f92a
evaldofelipe <contato@evaldofelipe.com>
2019-04-29 17:25:49 -0300

    fix shellcheck syntax

--- scripts/test_section_06_level1/6-1-14.sh
+++ scripts/test_section_06_level1/6-1-14.sh
@@ -8,25 +8,26 @@
 # The most common reason for a SGID program is to enable users to perform functions (such as changing their password) that require root privileges.
 
 set -o errexit
+set -o nounset
 
 declare gcp_binaries="24"
 declare azure_binaries="8"
 declare url_google="http://metadata/computeMetadata/v1/instance/hostname"
 status=0
 
-count_SGID=$(df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | wc -l)
+count_SGID=$(df --local -P | awk "{'if (NR!=1) print $6'}" | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | wc -l)
 
 curl -v -H Metadata-Flavor:Google $url_google -f > /dev/null 2>&1 || status=1
 
 if [[ "${status}x" == "0x" ]]; then
-  if [[ $count_SGID == $gcp_binaries ]]; then
+  if [[ $count_SGID == "$gcp_binaries" ]]; then
     exit 0
   else
     exit 1
   fi
 fi
 
-if [[ $count_SGID == $azure_binaries ]]; then
+if [[ $count_SGID == "$azure_binaries" ]]; then
   exit 0
 else
   exit 1
